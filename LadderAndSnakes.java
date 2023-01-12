10de52944912d2dcd907abddf1f605abc9140124
alexandrepayumo <alexpayumo@hotmail.com>
2023-01-12 16:57:09 -0500

    Created LadderAndSnakes and modififed PlayLadderAndSnakes

---
+++ LadderAndSnakes.java
@@ -0,0 +1,22 @@
+public class LadderAndSnakes {
+    private int[][] board = new int[10][10];
+    public void displayBoard() {
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board.length; j++){
+                board[i][j] = i*10 + j + 1;
+            }
+           }
+    
+           for (int i = board.length-1; i > -1; i--){
+            for (int j = board.length-1; j > -1; j--){
+                if (board[i][j] == 43){
+                    System.out.print("\u001b[31m" + board[i][j] + "\t" + "\033[0m");
+                }
+                else
+                    System.out.print(board[i][j] + "\t");
+            }
+            System.out.println();
+            System.out.println();
+           }
+    }
+}
