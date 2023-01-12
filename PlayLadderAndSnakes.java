d908fd9a6fc0b28c1012dcea571f2351a36de17d
benjamin-nguye <122396519+benjamin-nguye@users.noreply.github.com>
2023-01-12 16:38:13 -0500

    added board

--- PlayLadderAndSnakes.java
+++ PlayLadderAndSnakes.java
@@ -1,9 +1,29 @@
 public class PlayLadderAndSnakes {
     public static void main(String[] args) {
         //hello
-        System.out.println("hello");
-        System.out.println("hello #2");
+      
 
-        System.out.println("hello #3");
+       int[][] board = new int[10][10];
+
+       for (int i = 0; i < board.length; i++){
+        for (int j = 0; j < board.length; j++){
+            board[i][j] = i*10 + j + 1;
+        }
+       }
+
+       for (int i = board.length-1; i > -1; i--){
+        for (int j = board.length-1; j > -1; j--){
+            if (board[i][j] == 43){
+                System.out.print("\u001b[31m" + board[i][j] + "\t" + "\033[0m");
+            }
+            else
+                System.out.print(board[i][j] + "\t");
+        }
+        System.out.println();
+        System.out.println();
+       }
+
     }
 }
+
+
