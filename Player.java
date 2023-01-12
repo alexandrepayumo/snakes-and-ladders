74a5bdc284a5f4681d29dd313e410dbdd2b86668
alexandrepayumo <alexpayumo@hotmail.com>
2023-01-12 16:46:03 -0500

    Added player class

--- Player.java
+++ Player.java
@@ -1,3 +1,12 @@
 public class Player {
+    private int position;
+    private String name;
+    private String colour;
+
+    public Player(String name, String colour) {
+        this.position = 0;
+        this.name = name;
+        this.colour = colour;
+    }
     
 }
