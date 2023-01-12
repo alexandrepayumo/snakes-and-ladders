7250ef70d515c74447c7666a62b50629a4a98c38
benjamin-nguye <122396519+benjamin-nguye@users.noreply.github.com>
2023-01-12 17:01:59 -0500

    colours

--- Colour.java
+++ Colour.java
@@ -1,3 +1,17 @@
 public class Colour {
     
+    public static String Reset = "\u001b[0m";
+    public static String Red = "\u001b[31m";
+    public static String Blue = "\u001b[30m";
+    public static String Yellow = "\u001b[33m";
+    public static String Purple = "\u001b[35m";
+    public static String Green = "\u001b[32m";
+    public static String Cyan = "\u001b[36m";
+
+    public static String BackgroundRed = "\u001b[41m";
+    public static String BackgroundBlue = "\u001b[41m";
+    public static String BackgroundYellow = "\u001b[43m";
+
+
+
 }
