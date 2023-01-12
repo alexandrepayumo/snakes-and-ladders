8ad63eedf18a6789982d014f1badfe8c999f14dd
benjamin-nguye <122396519+benjamin-nguye@users.noreply.github.com>
2023-01-12 17:46:27 -0500

    modifications

---
+++ Board.java
@@ -0,0 +1,7 @@
+public class Board{
+    private int size = 10;
+    private int[][] Snakes;
+    private int[][] Ladders;
+
+    // create function for ladders and snakes
+}
--- LadderAndSnakes.java
+++ LadderAndSnakes.java
@@ -1,6 +1,12 @@
 
 public class LadderAndSnakes {
     private int[][] board = new int[10][10];
+    private int nbOfPlayers;
+    
+    public LadderAndSnakes(){
+
+    }
+    
     public void displayBoard() {
         for (int i = 0; i < board.length; i++){
             for (int j = 0; j < board.length; j++){
@@ -10,9 +16,9 @@ public class LadderAndSnakes {
     
            for (int i = board.length-1; i > -1; i--){
             for (int j = board.length-1; j > -1; j--){
-                if (board[i][j] == 43){
-                    System.out.print(Colour.Blue + board[i][j] + "\t" + Colour.Reset);
-                }
+                
+                if (board[i][j] == 100)
+                    System.out.print(Colour.Yellow + board[i][j] + Colour.Reset + "\t");
                 else
                     System.out.print(board[i][j] + "\t");
             }
