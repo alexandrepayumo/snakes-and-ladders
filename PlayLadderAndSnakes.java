8eb1fa98a9f40d5435f88b40a551f609244a1849
benjamin-nguye <122396519+benjamin-nguye@users.noreply.github.com>
2023-01-13 12:51:27 -0500

    updates

--- PlayLadderAndSnakes.java
+++ PlayLadderAndSnakes.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+// can probably combine all the import statements into 1 line
 
 public class PlayLadderAndSnakes {
     public static void main(String[] args) {
         
         Scanner kb = new Scanner(System.in);
-
-        String[] PlayerColours = {Colour.Red, Colour.Blue, Colour.Yellow, Colour.Purple, Colour.Green, Colour.Cyan};
 
         // Greetings
         // ask how many players
@@ -17,21 +18,68 @@ public class PlayLadderAndSnakes {
         System.out.print("\nStart by entering the number of players (maximum: 6): ");
         int nbOfPlayers = kb.nextInt();
 
-        Player[] playerList = new Player[nbOfPlayers];
+        if (nbOfPlayers > 2) {
+            System.out.println("Initialization was attempted for x members of players; however, this is only expected for and extended version of the game. Value will be set to 2");
+            nbOfPlayers = 2;
+        }
+        else if (nbOfPlayers < 2) {
+            System.out.println("Error: Cannot execute the game with less than 2 players! Will exit");
+            System.exit(0);
+        }
 
+        Player[] playerArray = new Player[nbOfPlayers];
+        
+        //Can get rid of these variables and hard code them to make the file shorter
+        String redText = "[r] for RED"; 
+        String blueText = "[b] for BLUE";
+        String greenText = "[g] for GREEN";
+        String yellowText = "[y] for YELLOW";
+        String purpleText = "[p] for PURPLE";
+        String cyanText = "[c] for CYAN";
+
+        //String colourTexts[] = new String[] {redText, blueText, greenText, yellowText, purpleText, cyanText};
+        List<String> colourList = new ArrayList<String>();
+        colourList.add(redText);
+        colourList.add(blueText);
+        colourList.add(greenText);
+        colourList.add(yellowText);
+        colourList.add(purpleText);
+        colourList.add(cyanText);
+
+
+        //Initializing playerList
         for (int i = 0; i < nbOfPlayers; i++){
             
             System.out.println();
             System.out.print("Player " + (i+1) + ", what is your name? ");
             String playerName = kb.next();
             System.out.println(playerName + ", what colour do you choose?");
-            System.out.print("Enter [r] for RED, "
-                                + "[b] for BLUE, "
-                                + "[g] for GREEN, "
-                                + "[y] for YELLOW, "
-                                + "[p] for PURPLE, "
-                                + "[c] for CYAN: ");
+            System.out.print("Enter");
+            for (int j=0; j<colourList.size(); j++) {
+                if (j == 0) {
+                    System.out.print(" ");
+                }
+                else {
+                    System.out.print(", ");
+                }
+                System.out.print(colourList.get(j));
+            }
+
             String playerColour = kb.next();
+            //could maybe use a dictionary to map each letter to a specific colour value instead of having all these if/else statements
+            //actually this would be much better because we need to dynamically change the number of available colours depending on what has already been chosen
+            if (playerColour.equals("b")) {
+
+            }
+            else if (playerColour.equals("")) {
+
+            }
+            else if (playerColour.equals("")) {
+
+            }
+            
+            //playerList.get(i.setColour());
+            colourList.remove(i+1);
         }
 
 
@@ -43,5 +91,4 @@ public class PlayLadderAndSnakes {
         
     }
 }
-
 
